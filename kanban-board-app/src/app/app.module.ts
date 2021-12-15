4f020fc2f2af4a901bfaf336fc3548bbe0211726
oPuDDingo <valerie.morviller@student.fhws.de>
2021-12-15 01:25:35 +0100

    Frontend toolbar

--- kanban-board-app/src/app/app.module.ts
+++ kanban-board-app/src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ToolbarComponent } from 'src/lib/ui/toolbar/toolbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ToolbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
